d74425885f0416b7cbea0923c12da93f669586ff
CalebChoo1 <S10205375@connect.np.edu.sg>
2021-05-05 22:25:18 +0800

    Practical 3 Completed

--- app/src/main/java/sg/edu/np/madpractical2_activity_s10205375/MainActivity.java
+++ app/src/main/java/sg/edu/np/madpractical2_activity_s10205375/MainActivity.java
@@ -3,11 +3,13 @@ package sg.edu.np.madpractical2_activity_s10205375;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     User user1;
@@ -25,18 +27,26 @@ public class MainActivity extends AppCompatActivity {
         TextView desc = findViewById((R.id.desc));
         user1.description = "Loren ipsum dolor sit amet consectetur\n adipiscing elit sed do eiusmod tempor\n incididunt ut labore et dolore magna aliqua";
         desc.setText(user1.getDesc());
-    }
 
-    public void method(View v) {
-        TextView follow_text = findViewById((R.id.followbtn));
-        if (!user1.followed) {
-            follow_text.setText("Unfollow");
-            user1.followed = true;
-        }
-        else {
-            follow_text.setText("Follow");
-            user1.followed = false;
-        }
-    }
+        Intent in = getIntent();
+        name.append(in.getStringExtra("ranInt"));
 
+        Button fllwBtn = findViewById(R.id.followbtn);
+        fllwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView follow_text = findViewById((R.id.followbtn));
+                if (!user1.followed) {
+                    follow_text.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
+                    user1.followed = true;
+                }
+                else {
+                    follow_text.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
+                    user1.followed = false;
+                }
+            }
+        });
+    }
 }
